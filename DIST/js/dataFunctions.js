a4707f7b63d14250c7a581f82b01c753d44e6dec
Bryan Samuels <bryan@Bryans-MacBook-Pro.local>
2022-06-02 19:45:38 -0500

    added serverless functions

--- DIST/js/dataFunctions.js
+++ DIST/js/dataFunctions.js
@@ -1,4 +1,3 @@
-const WEATHER_API_KEY = "a2be343223f13598ae591f7c372a8522";
 
 export const setLocationObject = (locationObj, coordsObj) =>{
     const {lat, lon, name, unit} = coordsObj;
@@ -17,67 +16,67 @@ export const getHomeLocation = () => {
 };
 
 export const getWeatherFromCoords = async (locationObj)=>{
-    const lat = locationObj.getLat();
-    const lon = locationObj.getLon();
-    const units = locationObj.getUnit();
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
-    try{
-        const weatherStream = await fetch(url);
-        const weatherJson = await weatherStream.json();
-        return weatherJson;
-    }catch (err){
-        console.error(err);
-    }
-
-
-    // const urlDataObj = {
-    //     lat: locationObj.getLat(),
-    //     lon: locationObj.getLon(),
-    //     units: locationObj.getUnit()
-    // };
-    // try {
-    //     const weatherStream = await fetch("./.netlify/serverless/get_weather", {
-    //         method: "POST",
-    //         body: JSON.stringify(urlDataObj)
-    //     });
+    // const lat = locationObj.getLat();
+    // const lon = locationObj.getLon();
+    // const units = locationObj.getUnit();
+    // const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+    // try{
+    //     const weatherStream = await fetch(url);
     //     const weatherJson = await weatherStream.json();
     //     return weatherJson;
-    // }catch(err){
-    //     console.log(err);
+    // }catch (err){
+    //     console.error(err);
     // }
+
+
+    const urlDataObj = {
+        lat: locationObj.getLat(),
+        lon: locationObj.getLon(),
+        units: locationObj.getUnit()
+    };
+    try {
+        const weatherStream = await fetch('./.netlify/functions/get_weather', {
+            method: "POST",
+            body: JSON.stringify(urlDataObj)
+        });
+        const weatherJson = await weatherStream.json();
+        return weatherJson;
+    }catch(err){
+        console.error(err);
+    }
 
 }   
 
 export const getCoordsFromApi = async (entryText, units) =>{
-    const regex = /^\d+$/g;
-    const flag = regex.test(entryText) ? "zip" : "q";
-    const url=`https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
-    const encodedUrl = encodeURI(url);
-    try {
-        const dataStream = await fetch(encodedUrl);
-        const jsonData = await dataStream.json();
-        //console.log(jsonData);
-        return jsonData;
-        
-    } catch (err){
-        console.error(err.stack);
-    }
-
-    // const urlDataObj = {
-    //     text: entryText,
-    //     units: units
-    // };
+    // const regex = /^\d+$/g;
+    // const flag = regex.test(entryText) ? "zip" : "q";
+    // const url=`https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+    // const encodedUrl = encodeURI(url);
     // try {
-    //     const dataStream = await fetch('./.netlify/functions/get_coords', {
-    //         method: "POST",
-    //         body: JSON.stringify(urlDataObj)
-    //     });
+    //     const dataStream = await fetch(encodedUrl);
     //     const jsonData = await dataStream.json();
+    //     //console.log(jsonData);
     //     return jsonData;
-    // } catch (err) {
-    //     console.error(err);
+        
+    // } catch (err){
+    //     console.error(err.stack);
     // }
-}
+
+    const urlDataObj = {
+        text: entryText,
+        units: units
+    };
+    try {
+        const dataStream = await fetch('./.netlify/functions/get_coords', {
+            method: "POST",
+            body: JSON.stringify(urlDataObj)
+        });
+        const jsonData = await dataStream.json();
+        return jsonData;
+    } catch (err) {
+        console.error(err);
+    }
+};
 
 export const cleanText = (text) =>{
     const regex = / {2,}/g;
